4c69e62d772bee1b1683413b4ebe4175b8d982b5
mugiwaratico <jacob.gonga@gmail.com>
2024-11-30 15:57:04 -0600

    Incorporacion tester y ejemplos de datos de entrada

--- testbench.v
+++ testbench.v
@@ -28,6 +28,51 @@ module UART_System_tb;
     always #10 clk_half = ~clk_half;
 
     initial begin
+        // Inicialización de señales
+        clk = 0;
+        clk_half = 0;
+        idle_uart1 = 1;
+        idle_uart2 = 1;
+        dataIn_uart1 = 8'b00111000;  // Primer dato para UART_1
+        dataIn_uart2 = 8'b01010101;  // Primer dato para UART_2
+
+        // Comienza la primera transmisión de UART_1 a UART_2
+        #10 
+        idle_uart1 = 0;  // Activar UART_1 para iniciar transmisión
+       
+        // Esperar para completar la transmisión de UART_1 a UART_2
+        #200;  // Ajusta el tiempo según el diseño para completar la transmisión
+
+        idle_uart1 = 1;
+
+        idle_uart2 = 0;
+
+        #200
+
+        // Datos para la segunda transmisión de UART_2 a UART_1
+               // Poner UART_1 en reposo
+               // Activar UART_2 para iniciar transmisión
+        dataIn_uart1 = 8'b11110000;  // Nuevo dato para la segunda transmisión de UART_1 a UART_2
+        dataIn_uart2 = 8'b00001111;  // Nuevo dato para la segunda transmisión de UART_2 a UART_1
+
+         // Comienza la primera transmisión de UART_1 a UART_2
+        idle_uart2 = 1;
+        #10 idle_uart1 = 0;  // Activar UART_1 para iniciar transmisión
+
+        // Esperar para completar la transmisión de UART_1 a UART_2
+        #200;  // Ajusta el tiempo según el diseño para completar la transmisión
+
+        idle_uart1 = 1;
+
+        idle_uart2 = 0;
+
+        #200
+
+        // Finalizar la simulación
+        $finish;
+    end
+
+    initial begin
         $dumpfile("tb.vcd");
         $dumpvars;
     end
