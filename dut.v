69ac73634ada0a58d1112e4dd0dbd3e6cdc890d1
Cesar Andres Fonseca Villegas <perryypipo2@gmail.com>
2024-12-01 08:59:40 -0600

    cambiar el nombre de variables

--- dut.v
+++ dut.v
@@ -4,12 +4,12 @@ module UART_System (
     input wire clk,
     input wire idle_uart1,
     input wire idle_uart2,
-    input wire [7:0] data_uart1,
-    input wire [7:0] data_uart2,
-    output wire tx_serial1,
-    output wire tx_serial2,
-    output wire [10:0] rx_serial1,
-    output wire [10:0] rx_serial2
+    input wire [7:0] dataIn_uart1,
+    input wire [7:0] dataIn_uart2,
+    output wire Tx_1,
+    output wire Tx_2,
+    output wire [10:0] Rx_1,
+    output wire [10:0] Rx_2
 );
 
     // Señales de interconexión entre los módulos UART_1 y UART_2
@@ -22,7 +22,7 @@ module UART_System (
     UART_1 uart1 (
         .UART1_CLK(clk),
         .IDLE_UART1(idle_uart1),
-        .data_in1(data_uart1),
+        .data_in1(dataIn_uart1),
         .Packet_In1(Packet_In1_dut),  // RX de UART_1 conectado al TX de UART_2
         .TX_2(tx_uart2_to_rx_uart1),
         .RX_Serial1(tx_uart2_to_rx_uart1),
@@ -33,7 +33,7 @@ module UART_System (
     UART_2 uart2 (
         .UART2_CLK(clk),
         .IDLE_UART2(idle_uart2),
-        .data_in2(data_uart2),
+        .data_in2(dataIn_uart2),
         .Packet_In2(Packet_In2_dut),  // RX de UART_2 conectado al TX de UART_1
         .TX_1(tx_uart1_to_rx_uart2),
         .RX_Serial2(tx_uart1_to_rx_uart2),
@@ -41,9 +41,9 @@ module UART_System (
     );
     
   // Asignaciones de salida para monitoreo en el testbench
-    assign tx_serial1 = tx_uart1_to_rx_uart2;
-    assign tx_serial2 = tx_uart2_to_rx_uart1;
-    assign rx_serial1 = Packet_In1_dut;
-    assign rx_serial2 = Packet_In2_dut;
+    assign Tx_1 = tx_uart1_to_rx_uart2;
+    assign Tx_2 = tx_uart2_to_rx_uart1;
+    assign Rx_1 = Packet_In1_dut;
+    assign Rx_2 = Packet_In2_dut;
 
 endmodule
